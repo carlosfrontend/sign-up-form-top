9a8c35164cd3d8e777f21d27acb42eb39ad9b47d
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2023-11-22 17:39:47 +0100

    Feat: Create response after submit the form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const firstName = document.querySelector("#firstname");
 const lastName = document.querySelector("#lastname");
+const email = document.querySelector('#email');
 const eye1 = document.querySelector("#eye1");
 const eye2 = document.querySelector("#eye2");
 const password1 = document.querySelector("#password1");
@@ -8,8 +9,6 @@ const form = document.getElementById("contact-form");
 const regex =
   /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
 const matchMessage = document.querySelector(".match-message");
-const responseTitle = document.querySelector(".response");
-
 firstName.focus(); // Puts the focus in the first field
 
 const showPassword1 = () => {
@@ -67,7 +66,31 @@ eye1.addEventListener("click", showPassword1);
 eye2.addEventListener("click", showPassword2);
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault()
+  e.preventDefault();
   document.write('<a href="index.html">Back</a>');
-  document.writeln(`<h1>Welcome ${firstName.value} ${lastName.value} !</h1>`)
+  document.writeln(`
+  <style>
+  body{
+  width:100%;
+  height:100%;
+  overflow:hidden;
+  background-color: rgb(148 6 6 / 76%);
+  background-image: url(../images/mountains.jpg);
+  background-repeat: no-repeat;
+  background-position: top left;
+  background-size: cover;
+  background-blend-mode: darken;
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+  color:white;
+  }
+  </style>
+  <img style="width:3vw;" src='../images/Wikipedia_Sourcing_Month_Logo.svg'/>
+  <h1>Welcome ${firstName.value} ${lastName.value} !</h1>
+  <h2>Thanks for register with us!<h2>
+  <p>You will receipe a message in <a href="mailto:${email.value}">${email.value}</a> in a few minutes.</p>
+  
+  `);
 });
